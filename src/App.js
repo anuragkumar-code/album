2de88558095953646c4d6dcfdc005977f1837f12
anuragkumar-code <kumar.mairwa@gmail.com>
2024-12-12 23:21:14 +0530

    completed registration and login page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import AuthPage from './pages/AuthPage';
+import RegistrationPage from './pages/Authentication/RegistrationPage';
 import Dashboard from './pages/Dashboard';
 import AlbumPage from './pages/AlbumPage';
 import CreateAlbumPage from './pages/CreateAlbumPage';
+import LoginPage from './pages/Authentication/LoginPage';
+
+import 'font-awesome/css/font-awesome.min.css';
+
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<AuthPage />} />
+        <Route path="/auth">
+          <Route path="login" element={<LoginPage />} />
+          <Route path="register" element={<RegistrationPage />} />
+        </Route>
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/album/:id" element={<AlbumPage />} />
         <Route path="/create-album" element={<CreateAlbumPage />} />
